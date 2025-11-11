984023fdd6a03e536c0ad4f9118a7fe92e119736
custom82 <62989759+custom82@users.noreply.github.com>
2025-11-11 16:24:17 +0100

    Add files via upload

--- src/pam_facial_auth.cpp
+++ src/pam_facial_auth.cpp
@@ -1,11 +1,11 @@
-// pam_facial_auth.cpp
+#include <security/pam_modules.h>  // Include PAM per l'autenticazione
+#include <security/pam_appl.h>     // Include PAM per il logging
+#include <syslog.h>                // Per i macro LOG_* (LOG_ERR, LOG_INFO, etc.)
 #include <string>
 #include <opencv2/opencv.hpp>
 #include <opencv2/face.hpp>
 #include <iostream>
 #include <filesystem>
-#include <security/pam_modules.h>  // Include PAM per l'autenticazione
-#include <security/pam_appl.h>    // Include PAM per il logging
 #include <fstream>                // Per std::ifstream
 #include <thread>                 // Per std::this_thread
 #include <chrono>                 // Per gestione del timeout
