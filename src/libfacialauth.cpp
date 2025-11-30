30fdae74647cb2bf39aed48933710af9970ca60b
Andrea Postiglione <62989759+custom82@users.noreply.github.com>
2025-11-30 16:54:44 +0100

    Add files via upload

--- src/libfacialauth.cpp
+++ src/libfacialauth.cpp
@@ -1236,10 +1236,9 @@ bool fa_test_user(const std::string &user,
             return false;
         }
 
-        if (cfg.debug) {
-            log_info(cfg, "Testing SFace model for user %s on %s", user.c_str(), dev.c_str());
-        }
-            user.c_str(), dev.c_str());
+
+        log_info(cfg, "Testing SFace model for user %s on %s", user.c_str(), dev.c_str());
+        user.c_str(), dev.c_str());
 
         double threshold = cfg.sface_threshold;
         if (threshold_override > 0.0)
