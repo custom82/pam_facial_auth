f1f4ddacbb46d0652c1eee14ad986b4a9b69c976
Andrea Postiglione <62989759+custom82@users.noreply.github.com>
2025-12-01 17:06:51 +0100

    Add files via upload

--- src/facial_capture.cpp
+++ src/facial_capture.cpp
@@ -1,6 +1,19 @@
 #include "libfacialauth.h"
+#include <iostream>
 
-int main(int argc, char *argv[])
+int facial_capture_main(int argc, char **argv);
+
+int main(int argc, char **argv)
 {
-    return facial_capture_main(argc, argv);
+    try {
+        return facial_capture_main(argc, argv);
+    }
+    catch (const cv::Exception &e) {
+        std::cerr << "[OpenCV ERROR] " << e.what() << std::endl;
+        return 1;
+    }
+    catch (const std::exception &e) {
+        std::cerr << "[ERROR] " << e.what() << std::endl;
+        return 1;
+    }
 }
